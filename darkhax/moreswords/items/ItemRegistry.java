ada11071d2a4541d6a7a822a32043f4ac4ae98f5
Darkhax <darklime@live.ca>
2013-07-04 15:53:12 -0600

    Language code removed. New hardcoded language files.

--- darkhax/moreswords/items/ItemRegistry.java
+++ darkhax/moreswords/items/ItemRegistry.java
@@ -32,14 +32,8 @@ public class ItemRegistry {
 	public static Config cfg;
 	public static String modId = Reference.id;
 	public static String name = Reference.name;
-
-	public static void load() {	
-		
-		addItems();
-		addNames();
-	}
 	
-	public static void addItems(){
+	public static void load(){
 		
 		blazeSword = new ItemCoreSword(cfg.blazeSwordID, EnumRegistry.blaze, "blaze").setUnlocalizedName("blaze");
 		bloodSword = new ItemCoreSword(cfg.bloodSwordID, EnumRegistry.blood, "blood").setUnlocalizedName("blood");
@@ -55,23 +49,5 @@ public class ItemRegistry {
 		aethersGuard= new ItemCoreSword(cfg.aethersGuardID, EnumRegistry.aether, "aether").setUnlocalizedName("aether");
 		witherBane = new ItemCoreSword(cfg.witherBaneID, EnumRegistry.wither, "wither").setUnlocalizedName("wither");
 		adminArk = new ItemCoreSword(cfg.adminArkID, EnumRegistry.admin, "admin").setUnlocalizedName("admin");
-	}
-	
-	public static void addNames(){
-		
-		LanguageRegistry.instance().addStringLocalization("item.blaze.name", "en_US", "Blaze Sword");
-		LanguageRegistry.instance().addStringLocalization("item.blood.name", "en_US", "Blood Sword");
-		LanguageRegistry.instance().addStringLocalization("item.bone.name", "en_US", "Bone Sword");
-		LanguageRegistry.instance().addStringLocalization("item.dragon.name", "en_US", "Draconic Blade");
-		LanguageRegistry.instance().addStringLocalization("item.eye.name", "en_US", "EyeEnd Sword");
-		LanguageRegistry.instance().addStringLocalization("item.glass.name", "en_US", "Glass Sword");
-		LanguageRegistry.instance().addStringLocalization("item.infinity.name", "en_US", "Infinity Sword");
-		LanguageRegistry.instance().addStringLocalization("item.lapis.name", "en_US", "Lapis Sword");
-		LanguageRegistry.instance().addStringLocalization("item.molten.name", "en_US", "Molten Edge");
-		LanguageRegistry.instance().addStringLocalization("item.master.name", "en_US", "Master Sword");
-		LanguageRegistry.instance().addStringLocalization("item.aqueous.name", "en_US", "Aqueous Blade");
-		LanguageRegistry.instance().addStringLocalization("item.aether.name", "en_US", "Aether's Guard");
-		LanguageRegistry.instance().addStringLocalization("item.wither.name", "en_US", "Wither Bane");
-		LanguageRegistry.instance().addStringLocalization("item.admin.name", "en_US", "Adminium Ark");
 	}
 }
