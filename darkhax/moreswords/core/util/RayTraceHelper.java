53c0802b562883dff8604eb121710eef39e5e9c4
Darkhax <darklime@live.ca>
2013-08-23 23:57:53 -0600

    Updated RayTrace helper to work with the 1.6.X update

--- darkhax/moreswords/core/util/RayTraceHelper.java
+++ darkhax/moreswords/core/util/RayTraceHelper.java
@@ -7,13 +7,13 @@ import net.minecraft.util.Vec3;
 import net.minecraft.util.Vec3Pool;
 import net.minecraft.world.World;
 
-public class RayTraceHelper
-{
-  public static MovingObjectPosition rayTrace(World world, EntityPlayer player, int distance)
-  {
-    Vec3 var1 = world.getWorldVec3Pool().getVecFromPool(player.posX, player.posY + player.getEyeHeight(), player.posZ);
-    Vec3 var2 = player.getLookVec();
-    Vec3 var3 = var1.addVector(var2.xCoord * distance, var2.yCoord * distance, var2.zCoord * distance);
-    return world.rayTraceBlocks_do_do(var1, var2, true, true);
+public class RayTraceHelper {
+	
+  public static MovingObjectPosition rayTrace(World world, EntityPlayer player, int length) {
+	  
+    Vec3 var4 = world.getWorldVec3Pool().getVecFromPool(player.posX, player.posY + player.getEyeHeight(), player.posZ);
+    Vec3 var5 = player.getLookVec();
+    Vec3 var6 = var4.addVector(var5.xCoord * length, var5.yCoord * length, var5.zCoord * length);
+    return world.clip(var4, var6);
   }
 }
