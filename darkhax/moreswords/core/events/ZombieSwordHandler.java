9798ca92bb598a0a33e11ac6ae3fe66e0306aa27
Darkhax <darklime@live.ca>
2013-09-11 21:51:15 -0400

    Fixed up some format issues. The zombie problem should now be fixed. I absolutely hate the way I did this code and I will be removing most of it once I get the EntityUtils prepared in my new core.

--- darkhax/moreswords/core/events/ZombieSwordHandler.java
+++ darkhax/moreswords/core/events/ZombieSwordHandler.java
@@ -22,65 +22,59 @@ public class ZombieSwordHandler {
 			range = Reference.random.nextIntII(1, 12);
 			if ((rand <= 0.2D) && ((event.entity instanceof EntityLiving)) && (event.entity instanceof EntityZombie)) {
 				EntityLiving living = (EntityLiving) event.entity;
+				
 				if (range == 1) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.bloodSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.bloodSword));				
 				}
 
 				if (range == 2) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.boneSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.boneSword));				
 				}
 
 				if (range == 3) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.dragonSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.dragonSword));					
 				}
 
 				if (range == 4) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.eyeEndSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.eyeEndSword));				
 				}
 
 				if (range == 5) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.glassSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.glassSword));				
 				}
 
 				if (range == 6) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.infinitySword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.infinitySword));				
 				}
 
 				if (range == 7) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.lapisSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.lapisSword));					
 				}
 
 				if (range == 8) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.moltenSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.moltenSword));					
 				}
 
 				if (range == 9) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.aqueousSword));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.aqueousSword));					
 				}
 
 				if (range == 10) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.aethersGuard));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.aethersGuard));					
 				}
 
 				if (range == 11) {
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.witherBane));
-					event.world.updateEntities();
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.witherBane));					
 				}
 
-				if (range == 12)
-					living.setCurrentItemOrArmor(0, new ItemStack(Items.blazeSword));
-				    event.world.updateEntities();
+				if (range == 12) {
+					living.setCurrentItemOrArmor(0, new ItemStack(Items.blazeSword));	
+				}
+				
+				event.world.updateEntity(living);
+				
 			    }
+			
 		   }
        }
 }
